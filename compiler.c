111722a8f92546ef3a825f232494f16abc3873f8
bonochof <ryos427@yahoo.co.jp>
2018-04-24 08:50:30 +0900

    add process to judge

--- compiler.c
+++ compiler.c
@@ -1,24 +1,55 @@
 #include<stdio.h>
 
+enum State
+{
+  ID, NUM, RSV, OPR, SEP
+};
+
+int isSeparate( char ch )
+{
+  switch( ch )
+  {
+  case ' ': case '\n':
+  case ',': case ':': case ';':
+  case '(': case ')': case '\'':
+  case '"': case '&': case '|':
+    return 1;
+  default:
+    return 0;
+  }
+}
+
 int main()
 {
-  int id = 0;
+  int i = 0, state;
   char ch, token[256];
+  char reserve[5][10] = {"if", "else", "for", "while", "return"};
   FILE *fp;
 
   fp = fopen("test.txt", "r");
 
   while( fscanf(fp, "%c", &ch) != EOF )
   {
-    if( ch != ' ' && ch != '\n' )
+    token[i++] = ch;
+
+    // first character
+    if( ch == 0 )
     {
-      token[id++] = ch;
+      if( ch >= '0' && ch <= '9' )
+      {
+        state = NUM;
+      }
     }
-    else 
+    else
     {
-      token[id] = '\0';
+      state = ID;
+    }
+
+    if( isSeparate( ch ) || isSeparate( token[0] ) )
+    {
+      token[i] = '\0';
       printf("%s\n", token);
-      id = 0;
+      i = 0;
     }
   }
 
